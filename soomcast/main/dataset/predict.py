e304d86fa46bdfd497c4daf26dfcaf76926a06c5
hhjun321 <hhjun321@gmail.com>
2017-06-12 14:06:36 +0900

    commit

--- soomcast/main/dataset/predict.py
+++ soomcast/main/dataset/predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import glob
 import pickle
  
 def start(engine, today, model_path):
@@ -19,9 +20,7 @@ def start(engine, today, model_path):
     del dataset_x['city']
     
     #select models
-    models  = pd.read_sql("SELECT * FROM model;", engine)
-    
-    model_names = models['model_name']
+    model_names = glob.glob(model_path+"\\*")
     
     #model_name
     col_list=[]
@@ -30,9 +29,9 @@ def start(engine, today, model_path):
     pred_list=[]
     
     #append predict value
-    for model_name in model_names:
+    for model_p in model_names:
         #open model
-        with open(model_name+".pkl", 'rb') as f:
+        with open(model_p, 'rb') as f:
             model = pickle.load(f)
             
             #predict...
@@ -40,18 +39,32 @@ def start(engine, today, model_path):
             pred_a = np.argmax(pred, axis=1)
             pred_a = str(pred_a[0]).replace('0','N').replace('1','Y')
     
+            #extract model_name
+            model_name = os.path.basename(model_p).replace('.pkl','')
+            
             col_list.append(model_name)
             pred_list.append(pred_a)
     
     #make pred dataframe        
-    final_df  = pd.DataFrame(list(zip (col_list,pred_list)))
+    pred_df  = pd.DataFrame(columns=(['model_code','pred']),data=list(zip (col_list,pred_list)))
     
-    #pred values
-    final_df = final_df.T[1:]
+    #select model_code    
+    models  = pd.read_sql("SELECT * FROM model;", engine)    
+    model_codes = models['model_name']
     
-    #pred columns name setting 
-    final_df.columns = col_list
-     
-    final_df['date'] = today
-    print('predict save..')
-    final_df.to_sql(name='pred', con=engine, if_exists = 'append', index=False, chunksize=1000000)
+    # insert data from each model_names
+    for model_code in model_codes:
+        #select model_code values
+        df = pred_df[pred_df['model_code'].str.contains(model_code) ]
+        
+        #pred values
+        final_df = df.T[1:]
+        
+        #pred columns name setting 
+        final_df.columns = [col.replace(model_code,"D") for col in df['model_code']]
+        final_df['model_name'] = model_code
+        final_df['date'] = today
+    
+        print('predict save..'+model_code)
+        final_df.to_sql(name='pred', con=engine, if_exists = 'append', index=False, chunksize=1000000)
+    
--- soomcast/main/dataset/predict2.py
+++ soomcast/main/dataset/predict2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import glob
 import h2o
 from h2o.estimators.glm import H2OGeneralizedLinearEstimator
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
@@ -34,9 +35,7 @@ def start(engine, today, model_path):
     df = h2o.H2OFrame(dataset_x)
     
     #select models
-    models  = pd.read_sql("SELECT * FROM model;", engine)
-    
-    model_names = models['model_name']
+    model_names = glob.glob(model_path+"\\*")
     
     #model_name
     col_list=[]
@@ -45,30 +44,39 @@ def start(engine, today, model_path):
     pred_list=[]
     
     #append predict value
-    for model_name in model_names:
+    for model_p in model_names:
         #open model
-        model = h2o.load_model(model_path+model_name)
+        model = h2o.load_model(model_p)
         
         h2o_pred = model.predict(df)
         h2o_pred = h2o.as_list(h2o_pred)
         h2o_pred = h2o_pred.predict
         pred_a = str(h2o_pred[0]).replace('0','N').replace('1','Y')
 
-  
+        model_name = os.path.basename(model_p)
         col_list.append(model_name)
         pred_list.append(pred_a)
     
     #shutdown h2o
     h2o.cluster().shutdown()
     #make pred dataframe        
-    final_df  = pd.DataFrame(list(zip (col_list,pred_list)))
+    pred_df  = pd.DataFrame(columns=(['model_code','pred']),data=list(zip (col_list,pred_list)))
     
-    #pred values
-    final_df = final_df.T[1:]
+    models  = pd.read_sql("SELECT * FROM model;", engine)
+    model_codes = models['model_name']
     
-    #pred columns name setting 
-    final_df.columns = col_list
-     
-    final_df['date'] = today
-    print('predict save..')
-    final_df.to_sql(name='pred', con=engine, if_exists = 'append', index=False, chunksize=1000000)
+    for model_code in model_codes:
+        
+        #subset model using model_code
+        df = pred_df[pred_df['model_code'].str.contains(model_code) ]
+        
+        #pred values
+        final_df = df.T[1:]
+        
+        #pred columns name setting 
+        final_df.columns = [col.replace(model_code,"D") for col in df['model_code']]
+        final_df['model_name'] = model_code
+        final_df['date'] = today
+        
+        print('predict save..'+model_code)
+        final_df.to_sql(name='pred', con=engine, if_exists = 'append', index=False, chunksize=1000000)
